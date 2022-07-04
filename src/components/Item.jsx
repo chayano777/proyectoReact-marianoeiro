e3386b8f5479bbf0c16ec8309d979e4fcaf4e509
chayano <marianoeiro@hotmail.com>
2022-07-04 08:20:52 -0300

    cambios de visual minimos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,11 +3,13 @@
 const Item = ({item}) => {
   return (
     
-    <div className="flex p-4 border rounded-lg shadow-sm">
+    <div className="flex p-4 border rounded-lg shadow-lg">
         <img src={item.thumbnail} alt="Imagen" />
         <div className="mx-4">
             {item.title}
-            <div>Stock Disponible: {item.available_quantity}</div>    
+            <div>Stock Disponible: {item.available_quantity}</div> 
+            <div className=""></div>
+               
         </div>     
     </div>
   )
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,8 +3,8 @@ import Item from "./Item"
 const ItemList = ({items}) => {
   return (
     <>
-        <div className="flex gap-10">
-        {items.slice(0,2).map((item, index)=> <Item key={index} item={item} />)}
+        <div className="flex flex-wrap gap-10">
+        {items.slice(0,4).map((item, index)=> <Item key={index} item={item} />)}
         </div>
     </>
   )

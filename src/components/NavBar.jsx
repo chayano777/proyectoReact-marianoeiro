b87fd6a9ddf5bcf8eec107f35f0f73b72bace3eb
chayano <marianoeiro@hotmail.com>
2022-07-19 00:19:31 -0300

    Fix en ruta inicio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,11 +9,11 @@ const NavBar = () => {
     <div className="p-2 border-b shadow-sm flex justify-between items-center bg-orange-100">
         <div className='container mx-auto ml-8 flex items-center gap-4'>
             <img className='rounded-full' width={100} src={logo} alt="Logo de Pichonos"/>
-            <Link to={"/"}className="text-3xl font-black text-red-500">Pichonos</Link>
+            <Link to={"/category/bebe"}className="text-3xl font-black text-red-500">Pichonos</Link>
         </div>
         <nav className='flex'>
             <ul className='flex gap-4 cursor-pointer' >
-                <li><Link to={"/"}>Inicio</Link></li>
+                <li><Link to={"/category/bebe"}>Inicio</Link></li>
                 <li><Link to="/category/ropa%20bebe">Ropa</Link></li>
                 <li><Link to="/category/accesorio%20bebe">Accesorios</Link></li>
             </ul>
